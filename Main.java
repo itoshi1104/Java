7236d6cf83c7e2b466478199d4d5a5d842d0d49c
itoshi1104 <ssj3itoshi1104@gmail.com>
2021-04-19 16:19:49 +0900

    基礎勉強1

--- Main.java
+++ Main.java
@@ -1,17 +1,69 @@
-//文字れ
+//文字列
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+  pageEncoding="UTF-8"%>
+<html lang="ja">
+
 class Main {
   public static void main(String[] args) {
     // "こんにちは"と"世界"を連結して出力してください
-   System.out.println("こんにちは" + "世界");
+    System.out.println("こんにちは" + "世界"); 
     
     // "38"と"19"を連結して出力してください
     System.out.println("38" + "19");
     
     // 38と19を足して出力してください
     System.out.println(38 + 19);
-   
+
+
+  }
+}
+
+//変数
+class Main {
+  public static void main(String[] args) {
+    // int型の変数numberを定義してください
+    int number;
+    
+    // 変数numberに3を代入してください
+    number = 3;
+    
+    // 変数numberを出力してください
+    System.out.println(number);
+
+    // String型の変数nameを定義してください
+    String name;
+    
+    // 変数nameに"Wanko"を代入してください
+    name = "Wanko";
+    
+    // 変数nameを出力してください
+    System.out.println(name);
+    
     
   }
 }
+
+//変数初期化
+
+class Main {
+  public static void main(String[] args) {
+    int number1 = 3;
+    
+    // int型の変数number2を定義し、7を代入してください
+   int number2 = 7; 
+    
+    // number1 * number2を出力してください
+    System.out.println(number1 * number2); 
+    
+    // 変数textに「プログラミングを勉強しよう」を代入してください
+    String text = "プログラミングを勉強しよう" ;
+    
+    // 「Progateで」と変数textを連結して出力してください
+   System.out.println("Progateで" + text); 
+    
+  }
+}
+
 
 
